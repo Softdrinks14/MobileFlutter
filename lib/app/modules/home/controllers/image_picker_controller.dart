6c4079520f1d1d9ade248093e18af35d67c4c0e0
Softdrinks14 <hisyambima5@gmail.com>
2024-10-02 10:10:02 +0700

    Replace ImagePicker can choose camera or gallery

--- lib/app/modules/home/controllers/image_picker_controller.dart
+++ lib/app/modules/home/controllers/image_picker_controller.dart
@@ -1,23 +1,51 @@
 import 'dart:io';
 
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImagePickerController extends GetxController {
-  Rx<File> image = File('').obs;
+  static ImagePickerController imgController = Get.find();
 
-  Future imagePicker() async {
-    try {
-      final imagePick =
-          await ImagePicker().pickImage(source: ImageSource.gallery);
-      if (imagePick == null) {
-        return;
-      }
-      final imageTemp = File(imagePick.path);
-      image.value = imageTemp;
-    } on PlatformException catch (e) {
-      return e;
+  var pickedFile = Rx<File?>(null);
+  File? get profileImage => pickedFile.value;
+  // Rx<File> image = File('').obs;
+
+  Future<void> imagePickerFromGallery() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? imageFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (imageFile != null) {
+      pickedFile.value = File(imageFile.path);
+      Get.snackbar("Profile Message", "Success Upload Picture");
+    } else {
+      Get.snackbar("Profile Message", "No image selected");
     }
   }
+
+  Future<void> imagePickerFromCamera() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? imageFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (imageFile != null) {
+      pickedFile.value = File(imageFile.path);
+      Get.snackbar("Profile Message", "Success Upload Picture");
+    } else {
+      Get.snackbar("Profile Message", "No image selected");
+    }
+  }
+
+  // Future imagePickerfromCamera() async {
+  //   try {
+  //     final imagePick =
+  //       await ImagePicker().pickImage(source: ImageSource.camera);
+  //     if (imagePick == null) {
+  //       return;
+  //     }
+  //     final imageTemp = File(imagePick.path);
+  //     image.value = imageTemp;
+  //   } on PlatformException catch (e) {
+  //     return e;
+  //   }
+  // }
 }
--- lib/app/modules/home/views/imagePicker.dart
+++ lib/app/modules/home/views/imagePicker.dart
@@ -3,6 +3,9 @@ import 'package:get/get.dart';
 import 'package:modul_1/app/modules/home/controllers/image_picker_controller.dart';
 
 class imagepicker extends StatelessWidget {
+  // var imagePicker = ImagePickerController.imgController;
+  final ImagePickerController controller = Get.put(ImagePickerController());
+
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(ImagePickerController());
@@ -14,25 +17,53 @@ class imagepicker extends StatelessWidget {
           Center(
             child: Obx(() {
               return Container(
-                  height: 500,
-                  width: 500,
-                  decoration: const BoxDecoration(color: Colors.black),
-                  child: controller.image.value.path == ''
-                      ? Icon(
-                          Icons.person,
-                          size: 200,
-                        )
-                      : Image.file(controller.image.value));
+                height: 500,
+                width: 500,
+                decoration: const BoxDecoration(color: Colors.white),
+                child: controller.pickedFile.value == null
+                    ? Icon(
+                        Icons.person,
+                        size: 200,
+                      )
+                    : Image.file(controller.pickedFile.value!),
+              );
             }),
           ),
           ElevatedButton(
-              onPressed: () {
-                controller.imagePicker();
+              onPressed: () async {
+                await chooseCamOrGal(context);
               },
               child:
                   const Text('Select Images', style: TextStyle(fontSize: 30)))
         ],
       )),
+    );
+  }
+
+  Future<void> chooseCamOrGal(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Select Image Source"),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop();
+                await controller.imagePickerFromGallery();
+              },
+              child: Text("Gallery"),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop();
+                await controller.imagePickerFromCamera();
+              },
+              child: Text("Camera"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
